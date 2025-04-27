2edf8bcee8e56e9a264ce7f51ccba538bd3d0547
Hugo Macedo de Carvalho <84153007+HMCarvalho-220@users.noreply.github.com>
2025-04-27 18:59:23 -0300

    Atualizar o EmpresaParceiraDTO.java

---
+++ Código/back/src/main/java/com/sistemaMonetario/Lab3/DTO/EmpresaParceiraDTO.java
@@ -0,0 +1,15 @@
+package com.sistemaMonetario.Lab3.DTO;
+
+import lombok.Data;
+
+@Data
+public class EmpresaParceiraDTO {
+ private Lond id;
+ private String nome;
+ private String email;
+ private String cnpj;
+ private String ramo;
+ private String descricao;
+ private String urlLogo;
+}
+ 
