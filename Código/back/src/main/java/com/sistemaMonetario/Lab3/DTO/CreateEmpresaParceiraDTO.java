cc2fd8463cd2a5885a2a6ee68ce6cabf8ea3df7e
Hugo Macedo de Carvalho <84153007+HMCarvalho-220@users.noreply.github.com>
2025-04-27 19:01:00 -0300

    Atualizar o CreateEmpresaParceiraDTO.java

---
+++ Código/back/src/main/java/com/sistemaMonetario/Lab3/DTO/CreateEmpresaParceiraDTO.java
@@ -0,0 +1,28 @@
+package com.sistemaMonetario.Lab3.DTO;
+
+import jakarta.validation.constraints.*;
+import lombok.Data;
+
+@Data
+public class CreateEmpresaParceiraDTO {
+    @NotBlank(message = "Nome é obrigatório")
+    private String nome;
+    
+    @Email(message = "Email inválido")
+    @NotBlank(message = "Email é obrigatório")
+    private String email;
+    
+    @NotBlank(message = "CNPJ é obrigatório")
+    @Pattern(regexp = "\\d{14}", message = "CNPJ deve conter 14 dígitos")
+    private String cnpj;
+    
+    @NotBlank(message = "Senha é obrigatória")
+    @Size(min = 6, message = "Senha deve ter no mínimo 6 caracteres")
+    private String senha;
+    
+    @NotBlank(message = "Ramo é obrigatório")
+    private String ramo;
+    
+    @Size(max = 500, message = "Descrição muito longa")
+    private String descricao;
+}
